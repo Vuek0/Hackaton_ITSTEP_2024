7135f3803e09cdd234880c30eb06b689572d7b0e
Vuek0 <vueko.eruko@gmail.com>
2024-03-25 23:47:52 +0500

    encrypt

--- public/main.js
+++ public/main.js
@@ -12,6 +12,41 @@ burger.addEventListener('click', ()=>{
         openOrNot--
     }
 })
+import md5 from 'md5';
+const encryptButton = document.querySelector("#encrypt__button");
+const encryptInput = document.querySelector("#encrypt__input");
+const radios = document.querySelectorAll(".encrypt__radio");
+const output = document.querySelector(".modal__output")
+if(encryptButton!=null){
+    encryptButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+        console.log("some");
+        radios.forEach(elem => {
+            if(elem.checked){
+                switch (elem.id) {
+                    case "md5":
+                        output.value = md5(encryptInput.value);
+                        openModal();
+                        break;
+                    case "crc32":
+                        
+                        break;
+                    case "sha1":
+                        
+                        break;
+                    case "sha256":
+                        
+                        break;
+                    case "sha512":
+                        
+                        break;
+                    default:
+                        break;
+                }
+            }
+        })
+    })
+}
 // variables
 
 const input = document.querySelector("#passwordInput");
