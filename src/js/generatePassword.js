63206a57111bd8be5443eb38f8e696065c98df29
Vuek0 <vueko.eruko@gmail.com>
2024-03-25 21:36:47 +0500

    password generator ready

--- src/js/generatePassword.js
+++ src/js/generatePassword.js
@@ -1,17 +1,3 @@
-let openOrNot = 0
-const burger = document.getElementById('burgerButton')
-const header = document.getElementById('header')
-burger.addEventListener('click', ()=>{
-    if(openOrNot == 0){
-        header.classList.add('open')
-        burger.classList.add('active')
-        openOrNot++
-    }else{
-        burger.classList.remove('active')
-        header.classList.remove('open')
-        openOrNot--
-    }
-})
 // variables
 
 const input = document.querySelector("#passwordInput");
