ac59ccb91beaeb13eb0ff7c5cfdc3b4c8f17ff9f
Vuek0 <vueko.eruko@gmail.com>
2024-03-26 01:15:13 +0500

    Encrypt READY

--- src/js/encrypt.js
+++ src/js/encrypt.js
@@ -1,35 +1,44 @@
-import md5 from 'md5';
 const encryptButton = document.querySelector("#encrypt__button");
 const encryptInput = document.querySelector("#encrypt__input");
 const radios = document.querySelectorAll(".encrypt__radio");
 const output = document.querySelector(".modal__output")
-if(encryptButton!=null){
-    encryptButton.addEventListener("click", (e)=>{
-        e.preventDefault();
-        console.log("some");
-        radios.forEach(elem => {
-            if(elem.checked){
-                switch (elem.id) {
-                    case "md5":
-                        output.value = md5(encryptInput.value);
-                        openModal();
-                        break;
-                    case "crc32":
-                        
-                        break;
-                    case "sha1":
-                        
-                        break;
-                    case "sha256":
-                        
-                        break;
-                    case "sha512":
-                        
-                        break;
-                    default:
-                        break;
-                }
-            }
-        })
-    })
+const modalCopyButton = document.querySelector("modal__copy")
+function createHash(method, str){
+    let hash = method(str);
+    output.textContent = hash;
+    navigator.clipboard.writeText(hash);
+    openModal();
 }
+    if(encryptButton!=null){
+        encryptButton.addEventListener("click", (e)=>{
+            e.preventDefault();
+            let str = encryptInput.value;
+            radios.forEach(elem => {
+                if(elem.checked){
+                    switch (elem.id) {
+                        case "md5":
+                            createHash(md5, str);
+                            break;
+                        case "crc32":
+                            // let hash1 = CRC32.str(str);
+                            // output.textContent = hash1;
+                            // navigator.clipboard.writeText(hash);
+                            openModal();
+                            break;
+                        case "sha1":
+                            createHash(sha1, str);
+                            break;
+                        case "sha256":
+                            createHash(sha256, str);
+                            break;
+                        case "sha512":
+                            createHash(sha512, str)
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            })
+        })
+    }
+
